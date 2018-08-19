4b6a488f899da2062a9967ee256932dae9b6d9b4
Marco Macias <mamacias3@espe.edu.ec>
2018-08-19 16:32:00 -0500

    colores y musica

---
+++ sphero_5/Assets/Scripts/AudioPear.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPear : MonoBehaviour {
+
+    private AudioSource _audioSource;
+    public static float[] _samples= new float[512];
+    public static float[] _frecBands = new float[8];
+    public static float[] _bandBuffer = new float[8];
+    float[] _freqBandHighest = new float[8];
+    float[] _bufferDecese = new float[8];
+    
+    public static float[] _audioBand = new float[8];
+    public static float[] _audioBandBuffer = new float[8];
+    void Start () {
+        _audioSource = GetComponent<AudioSource>();
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+        CreateAudioBands();
+	}
+
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+
+            if (_frecBands[i] > _freqBandHighest[i])
+            {
+                _freqBandHighest[i] = _frecBands[i];
+
+            }
+
+            if (_freqBandHighest[i] == 0)
+            {
+                _audioBandBuffer[i] = 0;
+            }
+            else
+            {
+                float aux = (_bandBuffer[i] / _freqBandHighest[i]);
+                if (aux < 0) { _audioBandBuffer[i] = 0; }
+                else
+                { _audioBandBuffer[i] = aux; }
+                _audioBand[i] = (_frecBands[i] / _freqBandHighest[i]);
+            }
+
+
+        }
+    }
+    //esta funcion obtiene los datos del espetro sonoro de la cancion
+    // y lo guarda en el array de muestras 
+    private void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples,0,FFTWindow.Blackman);
+    }
+    //Crea un buffer en el cual mientras no se ingrse un valor de valor de frecuencia mayor al valor del buffer el buffer ira restando 
+    //progresivamente el valor para que la barra no caiga inmediatamnte
+    void BandBuffer()
+    {
+        for(int g=0;g<8; g++)
+        {
+            if(_frecBands[g] > _bandBuffer[g])
+            {
+                _bandBuffer[g] = _frecBands[g];
+                _bufferDecese[g] = 0.005f;
+            }
+            if (_frecBands[g] < _bandBuffer[g])
+            {
+                _bandBuffer[g] -= _bufferDecese[g];
+                _bufferDecese[g] *= 1.2f;
+            }
+        }
+    }
+
+    private void MakeFrequencyBands()
+    {
+        int cont = 0;
+        for(int i=0;i<8;i++)
+        {
+            float averge = 0;
+            int _sampleCount = (int)Mathf.Pow(2, i) * 2;
+            if(i==7)
+            { _sampleCount += 2; }
+            for(int j=0;j<_sampleCount;j++)
+            {
+                averge += _samples[cont] *(cont+1);
+                cont++;
+            }
+
+            averge /= cont;
+            _frecBands[i] = averge * 10 ;
+        }
+    }
+}
---
+++ sphero_5/Assets/Scripts/ParamCube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour {
+
+    public int _band;
+    public float _startScale, _scaleMultipli;
+    public bool _useBuffer;
+    Material _material;
+	// Use this for initialization
+	void Start () {
+        _material = GetComponent<MeshRenderer>().materials[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(_useBuffer)
+        {
+            
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPear._bandBuffer[_band] * _scaleMultipli) + _startScale, transform.localScale.z);
+            Color _color = new Color(AudioPear._bandBuffer[_band], AudioPear._bandBuffer[_band], AudioPear._bandBuffer[_band]);
+            _material.SetColor("_EmissionColor", _color);
+        }
+        if (!_useBuffer)
+        {
+            //transform.localScale = new Vector3(transform.localScale.x, (AudioPear._audioBand[_band] * _scaleMultipli) + _startScale, transform.localScale.z);
+            //Color _color = new Color(AudioPear._audioBand[_band], AudioPear._audioBand[_band], AudioPear._audioBand[_band]);
+           // _material.SetColor("_EmissionColor", _color);
+        }
+    }
+}
