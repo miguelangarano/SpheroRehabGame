718ea9c4dfebb76c524029f7acb6306ee7bd39c0
Marco Macias <mamacias3@espe.edu.ec>
2018-08-19 19:30:17 -0500

    Menu y arte terminados

---
+++ sphero_5/Assets/Scripts/Menus/Botones.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Botones : MonoBehaviour {
+
+    //nombre de escena que deseas cargar
+    public string nextScene;
+
+	public void Inicio()
+    {
+        Application.LoadLevel(nextScene);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+   
+
+    
+}
